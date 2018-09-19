5601131e633405483dfa523b3c26c0c1b15b4008
SteinFluss <steinflussapps@gmail.com>
2018-09-19 23:14:41 +0200

    -

--- library/src/main/java/com/steinfluss/sf_fullscreendialog/FullScreenDialog.java
+++ library/src/main/java/com/steinfluss/sf_fullscreendialog/FullScreenDialog.java
@@ -2,20 +2,17 @@ package com.steinfluss.sf_fullscreendialog;
 
 import android.app.Activity;
 import android.app.Dialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class FullscreenDialog extends Dialog {
+public class FullScreenDialog extends Dialog {
 
     public static final String TAG = "FullScreenDialog";
 
     private int resId;
 
-    public FullscreenDialog(Activity act){
+    public FullScreenDialog(Activity act){
         //step 1, required. to stretch the dialog to full screen
         super(act, R.style.full_screen_dialog);
     }
@@ -36,7 +33,7 @@ public class FullscreenDialog extends Dialog {
         }catch (Exception ae){}
     }
 
-    public FullscreenDialog setContentViewRes(int resId){
+    public FullScreenDialog setContentViewRes(int resId){
         this.resId = resId;
         return this;
     }
